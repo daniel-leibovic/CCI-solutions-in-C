eac21f5a78bb64079c03f64986d8777eb2cedf0a
Daniel Leibovic <djleibovic@gmail.com>
2016-08-14 15:44:13 -0400

    bitArray

--- Chapter-1/1.1.c
+++ Chapter-1/1.1.c
@@ -2,31 +2,49 @@
 //See Q1v2.c for a space efficient version using bit-array
 
 #include <stdio.h>
- 
-int isUnique(char* string)
-{
-	//create array representing each of 256 possible ASCII chars, initialize all to 0
-	int ASCII[256] = {0};
-	int i;
-	char c;
-	
-	//place each char in your string into its unique place in ASCII array
-	//could optimize space by making ASCII array value type boolean
-	for(i = 0; c = string[i]; i++) {
-		printf("decimal value of %c: %d \n", c, c);
-		
-		//this character has been encountered before, and placed in ASCII array
-		if(ASCII[c]) {
-			printf("not unique!, ASCII[%d] = %d", c, c);
-			return;
-		}
-		
-		//set ASCII array value to char value
-		ASCII[c] = c;
-	}
-	printf("unique!");
+#include <stdbool.h>
+#include <string.h> 
+// int isUnique(char* string)
+// {
+// 	char mask[32];
+// 	//Rule of thumb: Never hardcode [data sizes]. 
+// 	memset(mask, 0, sizeof(mask));
+
+// }
+bool isUnique(char* str){
+    int i = 0;
+    char mask[32];
+    char c = 0;
+    char maskSpot = 0; //to represent spots 0 - 31 of the mask
+
+    memset(mask, 0, sizeof(mask));
+
+ 	while(c = str[i++]) {
+ 		//check if current letter has already been consumed, if bit (0-255) is flipped in bitArray
+ 		
+ 		//which maskSpot does the character belong to (maskSpots 0 - 31 represent bits 0 - 255)
+ 		maskSpot = c / 8; 
+ 		if ((mask[maskSpot] & (1 << (c % 8))) != 0)
+ 			return false;
+ 		else 
+ 			mask[maskSpot] |= (1 << (c % 8));
+ 	}
+
+ 	//all characters are unique. null strings also return 1.
+ 	return true;
+    
 }
-int main(void) {
-	isUnique("0hello");
+
+int main(int argc, char *argv[]) {
+	if(argc != 2) {
+		printf("Invalid arguments\n");
+		printf("Usage: %s test_string\n", argv[0]);
+		return 1;
+	}
+	bool testret = isUnique(argv[1]);
+	if(testret == true)
+		printf("\"%s\" has all unique characters!\n", argv[1]);
+	else
+		printf("\"%s\" does not have all unique characters!\n", argv[1]);
 	return 0;
 }
